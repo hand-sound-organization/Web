d2185fee86ff39b1a9c1dd3c2188c4fa8982743d
Rose666666 <993006297@qq.com>
2020-04-18 12:39:09 +0800

    添加了各区安全状态数据

--- models.py
+++ models.py
@@ -1,13 +1,23 @@
 from app import db
-from datetime import datetime
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password = db.Column(db.String(20))
     time = db.Column(db.DateTime)
-
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+class DistrictInfo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    status = db.Column(db.String)
+    time = db.Column(db.DateTime)
+    def __repr__(self):
+        return '<DistrictInfo %r>' % self.name
+
+
 
 
--- view.py
+++ view.py
@@ -3,23 +3,37 @@ from flask import render_template, request, jsonify
 from models import *
 import json
 import base64
+from datetime import datetime
 
 
 @app.route('/')
 def login():
-    print('xxxxxxxxxxxxxxxxxxxx')
     return render_template("login.html")
 
 
 @app.route('/HeatMap_chengdu', methods=['POST'])
 def homepage():
+    # 用户登录判定
     username = request.form['username']
     password = request.form['password']
     user = User.query.filter(User.username == username, User.password == password).all()
+    if username == 'luosida':
+        username = '罗嗣达'
+    elif username == 'jiangrenkai':
+        username = '姜人楷'
+    elif username == 'xiangguangyu':
+        username = '向广宇'
+    else:
+        username = '万斌朝硕'
+
     if not user:
         return "User's name and password are not correct! Please try again."
     else:
-        return render_template("start1.html", username=username)
+    # 加载成都市各区安全状态数据
+        districtInfo = DistrictInfo.query.all()
+        return render_template("start1.html",
+                               username=username,
+                               districtInfo = districtInfo)
 
 
 if __name__ == '__main__':
