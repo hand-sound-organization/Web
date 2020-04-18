6dc4a2d9d27cf61bfd14590de18dcf2520d3b48a
Rose666666 <993006297@qq.com>
2020-04-18 15:56:08 +0800

    添加了所有页面

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # Web
 web 掌音web端
 初次创建于2020.4.13
-## 框架使用情况
+## 框架&工具使用情况
+#####后端：
 1. 整体Web容器框架：Flask
 2. 数据库：SQLite
 3. 数据库控制插件：Flask-SQLAlchemy
 4. 页面模板框架：jinja2
 5. 数据库迁移插件：flask-migrate
+6. 数据库可视化软：件：SQLite Expert Personal 5.3
+#####前端：
+1. AdminLTE框架
+2. Jquery with Bootstrap
+3. AliMap 高德地图开放平台API
+
 
--- view.py
+++ view.py
@@ -10,7 +10,7 @@ from datetime import datetime
 def login():
     return render_template("login.html")
 
-
+# 热力图
 @app.route('/HeatMap_chengdu', methods=['POST'])
 def homepage():
     # 用户登录判定
@@ -34,6 +34,62 @@ def homepage():
         return render_template("start1.html",
                                username=username,
                                districtInfo = districtInfo)
+#热点图
+@app.route('/HeatMap_chengdu/HeatPointMap')
+def heatpoint():
+    return  render_template("start2.html")
+
+@app.route('/HeatMap_chengdu/Advise')
+def advise():
+    return  render_template("advise.html")
+
+@app.route('/HeatMap_chengdu/Attacklog')
+def attacklog():
+    return  render_template("attackLog.html")
+
+@app.route('/HeatMap_chengdu/wuhou')
+def wuhou():
+    return render_template("area1.html")
+
+@app.route('/HeatMap_chengdu/jinjiang')
+def jinjiang():
+    return render_template("area2.html")
+
+@app.route('/HeatMap_chengdu/qinyang')
+def qinyang():
+    return render_template("area3.html")
+
+@app.route('/HeatMap_chengdu/jinniu')
+def jinniu():
+    return render_template("area4.html")
+
+@app.route('/HeatMap_chengdu/chenghua')
+def chenghua():
+    return render_template("area5.html")
+
+@app.route('/HeatMap_chengdu/longquanyi')
+def longquanyi():
+    return render_template("area6.html")
+
+@app.route('/HeatMap_chengdu/wenjiang')
+def wenjiang():
+    return render_template("area7.html")
+
+@app.route('/HeatMap_chengdu/xindu')
+def xindu():
+    return render_template("area8.html")
+
+@app.route('/HeatMap_chengdu/qinbaijiang')
+def qinbaijiang():
+    return render_template("area9.html")
+
+@app.route('/HeatMap_chengdu/shuangliu')
+def shuangliu():
+    return render_template("area10.html")
+
+@app.route('/HeatMap_chengdu/pidu')
+def pidu():
+    return render_template("area11.html")
 
 
 if __name__ == '__main__':
