1228f0a7e6773ed4c36f9a8543dd7483371d5bd9
Rose666666 <993006297@qq.com>
2020-04-18 11:09:49 +0800

    添加了用户登录模块，用户名为名字拼音全拼，密码为全拼+123

--- view.py
+++ view.py
@@ -1,18 +1,24 @@
 from app import app, db
 from flask import render_template, request, jsonify
-from models import User
+from models import *
 import json
 import base64
 
+
 @app.route('/')
 def login():
-
     return render_template("login.html")
 
 
-@app.route('/HeatMap_chengdu')
-def register():
-    return render_template("start1.html")
+@app.route('/HeatMap_chengdu', methods=['POST'])
+def homepage():
+    username = request.form['username']
+    password = request.form['password']
+    user = User.query.filter(User.username == username, User.password == password).all()
+    if not user:
+        return "User's name and password are not correct! Please try again."
+    else:
+        return render_template("start1.html", username=username)
 
 
 if __name__ == '__main__':
